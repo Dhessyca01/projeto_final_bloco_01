48acc3920abccd248c480cc75e585c872098c4b6
Dhessyca01 <157776978+Dhessyca01@users.noreply.github.com>
2024-04-03 14:59:07 -0300

    Add files via upload

---
+++ Produtocontroller.ts
@@ -0,0 +1,68 @@
+import { Produto } from "../model/Principal";
+import { ProdutoRepository } from "../model/02_Model_Repository/Repository";
+
+
+export class Produtocontroller implements ProdutoRepository{
+      
+
+    private listaProdutos: Array<Produto> = new Array<Produto>();
+
+
+    public id: number = 0;
+
+
+    listarTodas(): void {
+        for (let produto of this.listaProdutos){
+             produto.visualizar();
+        }
+     }
+
+
+    procurarPorId(id: number): void {
+        let buscaproduto = this.buscarNoArray(id);
+
+        if(buscaproduto !== null)
+            buscaproduto.visualizar()
+        else
+            console.log("\no Produto não foi Encontrado!")
+    }
+
+    cadastrar(produto: Produto): void {
+        this.listaProdutos.push(produto);
+        console.log("O produto foi adicionado!")
+    }
+
+    atualizar(produto: Produto): void {
+        let buscaProduto = this.buscarNoArray(produto.id);
+
+        if(buscaProduto !== null){
+            this.listaProdutos[this.listaProdutos.indexOf(buscaProduto)] = produto;
+            console.log(`O produto número ${produto.id} foi Atualizado com êxito!`)
+        }else
+            console.log("\nO produto não foi Encontrado!")
+    }
+
+    deletar(id: number): void {
+        let buscaProduto = this.buscarNoArray(id);
+
+        if(buscaProduto !== null){
+            this.listaProdutos.splice(this.listaProdutos.indexOf(buscaProduto), 1)
+            console.log(`O produto número ${id} foi Excluído com êxito!`)
+        }else
+            console.log("\nO produto não foi Encontrado!")
+    }
+
+
+    public gerarId(): number{
+        return ++ this.id
+    }
+    
+    public buscarNoArray(id: number): Produto | null{
+        for (let produto of this.listaProdutos){
+            if (produto.id === id)
+                return produto;
+       }
+
+       return null;
+    }
+}
